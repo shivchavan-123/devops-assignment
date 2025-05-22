0df5d116ed2109cc3d4b03ac0a522844934fcc4f
shiv <shivchavan2612@gmail.com>
2025-05-23 01:09:45 +0530

    1st commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+import logging
+
+app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
+
+@app.route("/")
+def home():
+    app.logger.info("Home page accessed")
+    return "Hello, World!"
+
+@app.route("/healthz")
+def healthz():
+    return "OK", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
---
+++ main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region = var.aws_region
+
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+
+module "vpc" {
+  source = "./modules/network"
+
+  project              = "devops-assignment"
+  vpc_cidr             = "10.0.0.0/16"
+  public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnet_cidrs = ["10.0.101.0/24", "10.0.102.0/24"]
+  azs                  = ["us-east-1a", "us-east-1b"]
+}
+
+/* module "ecs" {
+  source = "./modules/ecs"  # Adjust if module is elsewhere
+
+  aws_region           = var.aws_region
+  ecr_repo_url         = ""
+  image_tag            = "latest"
+
+  private_subnets         = module.vpc.private_subnets
+  ecs_security_group_id   = aws_security_group.ecs_tasks_sg.id
+  target_group_arn        = aws_lb_target_group.app_tg.arn
+}
+*/
+
+
