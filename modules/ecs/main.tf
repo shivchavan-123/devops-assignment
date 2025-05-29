66aab940774c64e33365b329e4d5bfc5f8be8f69
shivchavan-123 <shivshankar.chavan@armakuni.com>
2025-05-29 19:51:50 +0530

    Added ASG AND dynamic env setup to my workflow

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -95,3 +95,47 @@ resource "aws_ecs_service" "this" {
 
   depends_on = [aws_iam_role_policy_attachment.execution_policy]
 }
+
+
+resource "aws_appautoscaling_target" "ecs" {
+  max_capacity       = 4
+  min_capacity       = 1
+  resource_id        = "service/${var.cluster_name}/${aws_ecs_service.this.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+resource "aws_appautoscaling_policy" "scale_up" {
+  name               = "scale-up"
+  policy_type        = "StepScaling"
+  resource_id        = aws_appautoscaling_target.ecs.resource_id
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+
+  step_scaling_policy_configuration {
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = 60
+    metric_aggregation_type = "Average"
+
+    step_adjustment {
+      scaling_adjustment = 1
+      metric_interval_lower_bound = 0
+    }
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "cpu_high" {
+  alarm_name          = "cpu-alarm-for-ecs"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 70
+  dimensions = {
+    ClusterName = var.cluster_name
+    ServiceName = aws_ecs_service.this.name
+  }
+  alarm_actions = [aws_appautoscaling_policy.scale_up.arn]
+}
