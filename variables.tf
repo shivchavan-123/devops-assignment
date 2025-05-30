38cf7542738095cfa891b77c040e0030bc619901
shivchavan-123 <shivshankar.chavan@armakuni.com>
2025-05-30 14:37:19 +0530

    Separate env variable has been added in workflow

--- variables.tf
+++ variables.tf
@@ -18,3 +18,21 @@ variable "image_uri" {
   description = "The ECR image URI built from CI pipeline"
   type        = string
 }
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+}
+variable "private_subnet_cidrs" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+}
+
+variable "azs" {
+  description = "List of availability zones"
+  type        = list(string)
+  
+}
